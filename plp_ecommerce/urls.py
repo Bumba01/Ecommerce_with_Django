fd2125a556fdf4feb847400fd8a2a692e41ee5ac
bumba01 <faithe396@gmail.com>
2024-06-18 01:12:49 -0700

    added css files

--- plp_ecommerce/urls.py
+++ plp_ecommerce/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = 'plp_ecommerce'
+
 urlpatterns = [
     path('', views.home, name='home'),
-    
-
+    path('products/', views.product_list, name='product_list'),
+    path('products/<int:pk>/', views.product_detail, name='product_details'),
+    path('customers/', views.customer_list, name='customer_list'),
+    path('customers/<int:pk>', views.customer_detail, name='customer_detail')
 
 
 ]
